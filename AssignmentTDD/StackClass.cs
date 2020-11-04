e2fccd47fd319f6623d02802451ed0c916f7c8ae
hankzhu123 <czhu2@washcoll.edu>
2020-11-03 23:27:11 -0700

    Add project files.

---
+++ AssignmentTDD/StackClass.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+
+namespace AssignmentTDD
+{
+    class StackClass
+    {
+        private int[] m_stack;
+
+        private StackClass() { }
+
+        public StackClass(int[] stack)
+        {
+            m_stack = stack;
+        }
+
+        public int[] Stack
+        {
+            get { return m_stack; }
+        }
+
+        public void Push(int number)
+        {
+            int[] newS = new int[m_stack.Length+1];
+
+            for (int i = 0; i < m_stack.Length; i++)
+            {
+                newS[i] = m_stack[i];
+            }
+
+            newS[m_stack.Length] = number;
+
+            m_stack = newS;
+            
+        }
+
+        public void Pop()
+        {
+            if (m_stack.Length == 0)
+            {
+                throw new ArgumentOutOfRangeException("This is an empty stack");
+            }
+
+            int[] newS = new int[m_stack.Length - 1];
+
+            for (int i = 0; i < m_stack.Length-1; i++)
+            {
+                newS[i] = m_stack[i];
+            }
+
+            m_stack = newS;
+
+        }
+
+        public void Peek()
+        {
+            if (m_stack.Length == 0)
+            {
+                throw new ArgumentOutOfRangeException("This is an empty stack");
+            }
+
+            Console.WriteLine("Top of the Stack is: ", m_stack[m_stack.Length]);
+
+        }
+
+
+        public static void Main()
+        {
+            int[] newStack = { 1, 2, 3, 4 };
+            StackClass sc = new StackClass(newStack);
+
+            sc.Pop();
+            sc.Push(100);
+
+            foreach (int i in sc.Stack)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
